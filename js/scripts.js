f6eaae7031fce1c0e9e2c8a49c3a3a6e0bf583af
Jory Smith <JorySmith@users.noreply.github.com>
2023-07-15 09:39:24 -0500

    Added dark mode

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+$("a.prevent-default").click(function (e) {
+  // Cancel the default action
+  e.preventDefault();
+});
+
+// Dark mode selector
+const toggler = document.getElementById('toggler');
+const headOne = document.getElementsByTagName("h1");
+const headFive = document.getElementsByTagName("h5");
+const aTags = document.getElementsByTagName("a");
+const accordionBody = document.getElementsByClassName("accordion-body");
+const accordionButton = document.getElementsByClassName("accordion-button");
+const lists = document.getElementsByTagName("li");
+const spans = document.getElementsByTagName("span");
+
+toggler.addEventListener('change', () => {
+	document.body.classList.toggle('dark-mode');
+	for (let i=0; i< headOne.length; i++) {
+    headOne[i].classList.toggle('dark-mode')
+  }
+	for (let i=0; i< headFive.length; i++) {
+    headFive[i].classList.toggle('dark-mode')
+  }
+	for (let i=0; i< aTags.length; i++) {
+    aTags[i].classList.toggle('dark-mode')
+  }
+  for (let i=0; i< accordionBody.length; i++) {
+    accordionBody[i].classList.toggle('dark-mode')
+  }
+  for (let i=0; i< accordionButton.length; i++) {
+    accordionButton[i].classList.toggle('dark-mode')
+  }
+	for (let i=0; i< lists.length; i++) {
+    lists[i].classList.toggle('dark-mode')
+  }
+	for (let i=0; i< spans.length; i++) {
+    spans[i].classList.toggle('dark-mode')
+  }	
+});
