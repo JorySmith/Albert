26153568ea095db2e3127b54b44b76dfe3f37080
Jory Smith <JorySmith@users.noreply.github.com>
2023-07-16 07:57:19 -0500

    Added Fears and fixed dark mode bugs

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $("a.prevent-default").click(function (e) {
 });
 
 // Dark mode selector
-const toggler = document.getElementById('toggler');
+const toggler = document.getElementById("toggler");
 const headOne = document.getElementsByTagName("h1");
 const headFive = document.getElementsByTagName("h5");
 const aTags = document.getElementsByTagName("a");
@@ -17,30 +17,30 @@ const ulSubheading = document.getElementsByClassName("ul-subheading");
 const lists = document.getElementsByTagName("li");
 const spans = document.getElementsByTagName("span");
 
-toggler.addEventListener('change', () => {
-	document.body.classList.toggle('dark-mode');
-	for (let i=0; i< headOne.length; i++) {
-    headOne[i].classList.toggle('dark-mode')
+toggler.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode");
+  for (let i = 0; i < headOne.length; i++) {
+    headOne[i].classList.toggle("dark-mode");
   }
-	for (let i=0; i< headFive.length; i++) {
-    headFive[i].classList.toggle('dark-mode')
+  for (let i = 0; i < headFive.length; i++) {
+    headFive[i].classList.toggle("dark-mode");
   }
-	for (let i=0; i< aTags.length; i++) {
-    aTags[i].classList.toggle('dark-mode')
+  for (let i = 0; i < aTags.length; i++) {
+    aTags[i].classList.toggle("dark-mode");
   }
-  for (let i=0; i< accordionBody.length; i++) {
-    accordionBody[i].classList.toggle('dark-mode')
+  for (let i = 0; i < accordionBody.length; i++) {
+    accordionBody[i].classList.toggle("dark-mode");
   }
-  for (let i=0; i< accordionButton.length; i++) {
-    accordionButton[i].classList.toggle('dark-mode')
+  for (let i = 0; i < accordionButton.length; i++) {
+    accordionButton[i].classList.toggle("dark-mode");
   }
-  for (let i=0; i< ulSubheading.length; i++) {
-    ulSubheading[i].classList.toggle('dark-mode')
+  for (let i = 0; i < ulSubheading.length; i++) {
+    ulSubheading[i].classList.toggle("dark-mode");
   }
-	for (let i=0; i< lists.length; i++) {
-    lists[i].classList.toggle('dark-mode')
+  for (let i = 0; i < lists.length; i++) {
+    lists[i].classList.toggle("dark-mode");
   }
-	for (let i=0; i< spans.length; i++) {
-    spans[i].classList.toggle('dark-mode')
-  }	
+  for (let i = 0; i < spans.length; i++) {
+    spans[i].classList.toggle("dark-mode");
+  }
 });
